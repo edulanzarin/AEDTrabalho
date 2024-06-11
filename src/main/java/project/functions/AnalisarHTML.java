2121978c9c8b03998b9b6605316ceea6a7d4356d
edulanzarin <edulanzarin@outlook.com>
2024-06-11 10:48:49 -0300

    SingletonTags modificado

--- src/main/java/project/functions/AnalisarHTML.java
+++ src/main/java/project/functions/AnalisarHTML.java
@@ -4,49 +4,12 @@ import project.core.*;
 
 public class AnalisarHTML {
     private String conteudo;
-    ListaEncadeada<String> tagNames = new ListaEncadeada<>();
+
+    public AnalisarHTML() {
+        this.conteudo = null;
+    }
 
     public AnalisarHTML(String conteudo) {
         this.conteudo = conteudo;
     }
-
-    public ListaEncadeada<String> retirarTags() {
-        boolean insideTag = false;
-        StringBuilder currentTagName = new StringBuilder();
-    
-        for (int i = 0; i < conteudo.length(); i++) {
-            char currentChar = conteudo.charAt(i);
-            if (currentChar == '<') {
-                insideTag = true;
-                currentTagName = new StringBuilder(); 
-            } else if (currentChar == '>') {
-                insideTag = false;
-                if (currentTagName.length() > 0) {
-                    tagNames.inserir(currentTagName.toString());
-                }
-            } else if (!insideTag) {
-                currentTagName.append(currentChar);
-            }
-        }
-    
-        return tagNames;
-    }
-
-    public ListaEncadeada<String> removerSingletonTags(ListaEncadeada<String> tagNames) {
-        SingletonTags singletonTags = new SingletonTags();
-        ListaEncadeada<String> modifiedTagNames = new ListaEncadeada<>();
-    
-        // Iterate through the extracted tag names
-        for (int i = 0; i < tagNames.getTamanho(); i++) {
-            String tagName = tagNames.obterNo(i).getInfo();
-            // Check if the tag is a singleton tag
-            if (!singletonTags.isSingletonTag(tagName)) {
-                // If not a singleton tag, add it to the modified list
-                modifiedTagNames.inserir(tagName);
-            }
-        }
-    
-        return modifiedTagNames;
-    }
-    
 }
--- src/main/java/project/functions/SingletonTags.java
+++ src/main/java/project/functions/SingletonTags.java
@@ -1,28 +1,18 @@
 package project.functions;
 
-import project.core.ListaEncadeada;
-
 public class SingletonTags {
     private static final String[] SINGLETON_TAGS = {
         "meta", "base", "br", "col", "command", "embed", "hr", "img",
         "input", "link", "param", "source", "!DOCTYPE"
     };
-    private ListaEncadeada<String> singTagsEncontradas;
-
-    public SingletonTags() {
-        this.singTagsEncontradas = new ListaEncadeada<>();
-    }
 
     public boolean isSingletonTag(String tag) {
+        tag = tag.replaceAll("[<>]", "");
         for (String singletonTag : SINGLETON_TAGS) {
-            if (tag.equals(singletonTag)) {
+            if (tag.equalsIgnoreCase(singletonTag)) {
                 return true;
             }
         }
         return false;
-    }
-
-    public ListaEncadeada<String> getSingTagsEncontradas() {
-        return singTagsEncontradas;
     }
 }
