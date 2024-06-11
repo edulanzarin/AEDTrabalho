2c4c5e1b1457fa4aabd22d4dbb8b5b78f5f8dacc
Eduardo Lanzarin <edulanzarin@outlook.com>
2024-06-11 18:46:32 -0300

    commit

--- src/main/java/project/functions/AnalisarHTML.java
+++ src/main/java/project/functions/AnalisarHTML.java
@@ -4,49 +4,46 @@ import project.core.*;
 
 public class AnalisarHTML {
     private String conteudo;
-    ListaEncadeada<String> tagNames = new ListaEncadeada<>();
+    ListaEncadeada<String> tagsLista = new ListaEncadeada<>();
 
     public AnalisarHTML(String conteudo) {
         this.conteudo = conteudo;
     }
 
     public ListaEncadeada<String> retirarTags() {
-        boolean insideTag = false;
-        StringBuilder currentTagName = new StringBuilder();
+        boolean isTag = false;
+        StringBuilder tagAtual = new StringBuilder();
     
         for (int i = 0; i < conteudo.length(); i++) {
-            char currentChar = conteudo.charAt(i);
-            if (currentChar == '<') {
-                insideTag = true;
-                currentTagName = new StringBuilder(); 
-            } else if (currentChar == '>') {
-                insideTag = false;
-                if (currentTagName.length() > 0) {
-                    tagNames.inserir(currentTagName.toString());
+            char caractereAtual = conteudo.charAt(i);
+            if (caractereAtual == '<') {
+                isTag = true;
+                tagAtual = new StringBuilder(); 
+            } else if (caractereAtual == '>') {
+                isTag = false;
+                if (tagAtual.length() > 0) {
+                    tagsLista.inserir(tagAtual.toString());
                 }
-            } else if (!insideTag) {
-                currentTagName.append(currentChar);
+            } else if (!isTag) {
+                tagAtual.append(caractereAtual);
             }
         }
     
-        return tagNames;
+        return tagsLista;
     }
 
-    public ListaEncadeada<String> removerSingletonTags(ListaEncadeada<String> tagNames) {
+    public ListaEncadeada<String> removerSingletonTags(ListaEncadeada<String> tagsLista) {
         SingletonTags singletonTags = new SingletonTags();
-        ListaEncadeada<String> modifiedTagNames = new ListaEncadeada<>();
+        ListaEncadeada<String> tagsNormais = new ListaEncadeada<>();
     
-        // Iterate through the extracted tag names
-        for (int i = 0; i < tagNames.getTamanho(); i++) {
-            String tagName = tagNames.obterNo(i).getInfo();
-            // Check if the tag is a singleton tag
-            if (!singletonTags.isSingletonTag(tagName)) {
-                // If not a singleton tag, add it to the modified list
-                modifiedTagNames.inserir(tagName);
+        for (int i = 0; i < tagsLista.getTamanho(); i++) {
+            String tag = tagsLista.obterNo(i).getInfo();
+            if (!singletonTags.isSingletonTag(tag)) {
+                tagsNormais.inserir(tag);
             }
         }
     
-        return modifiedTagNames;
+        return tagsNormais;
     }
     
 }
