5f8d53a698d03250f26a2bea3e5bf33a9c7899e5
Eduardo Lanzarin <edulanzarin@outlook.com>
2024-06-17 21:43:30 -0300

    Criação do javadoc

--- src/main/java/project/model/exceptions/NenhumArquivoSelecionadoException.java
+++ src/main/java/project/model/exceptions/NenhumArquivoSelecionadoException.java
@@ -1,10 +1,22 @@
 package project.model.exceptions;
 
+/**
+ * Exceção lançada quando nenhum arquivo foi selecionado para uma operação que requer um arquivo.
+ */
 public class NenhumArquivoSelecionadoException extends RuntimeException {
+
+    /**
+     * Constrói uma nova exceção com uma mensagem padrão.
+     */
     public NenhumArquivoSelecionadoException() {
         super("Nenhum arquivo selecionado.");
     }
 
+    /**
+     * Constrói uma nova exceção com uma mensagem especificada.
+     *
+     * @param mensagem a mensagem detalhada.
+     */
     public NenhumArquivoSelecionadoException(String mensagem) {
         super(mensagem);
     }
